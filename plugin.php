250edcf031c592d68005a1744f59dcab88a08187
Tim Crockford <tim@crockford.id.au>
2014-10-15 21:34:10 +1100

    API alignment fix
    Updated the “update” call to allow the $shorturl parameter to be either
    a URL or a keyword. Also fixed some spacing issues and increased the
    revision number to account for the update. Thanks to YOzaz for the fix!

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
      Plugin Name: Update Shortened URL
      Plugin URI: https://github.com/timcrockford/yourls-api-edit-url
      Description: Define a custom API action 'update' and 'geturl'
-     Version: 0.2
+     Version: 0.2.1
      Author: Tim Crockford
      Author URI: http://codearoundcorners.com/
      */
@@ -13,19 +13,19 @@
     yourls_add_filter( 'api_action_geturl', 'api_edit_url_get' );
     
     function api_edit_url_update() {
-        if( !isset( $_REQUEST['shorturl'] ) ) {
+        if ( ! isset( $_REQUEST['shorturl'] ) ) {
             return array(
                 'statusCode' => 400,
-	    	'status' => 'fail',
+                'status'     => 'fail',
                 'simple'     => "Need a 'shorturl' parameter",
                 'message'    => 'error: missing param',
             );
         }
 
-        if( !isset( $_REQUEST['url'] ) ) {
+        if ( ! isset( $_REQUEST['url'] ) ) {
             return array(
                 'statusCode' => 400,
-	         'status' => 'fail',
+                'status'     => 'fail',
                 'simple'     => "Need a 'url' parameter",
                 'message'    => 'error: missing param',
             );
@@ -34,11 +34,17 @@
         $shorturl = $_REQUEST['shorturl'];
         $url = urldecode($_REQUEST['url']);
 
-        if( ! yourls_is_shorturl( $shorturl ) ) {
+        if ( yourls_get_protocol( $shorturl ) ) {
+            $keyword = yourls_get_relative_url( $shorturl );
+        } else {
+            $keyword = $shorturl;
+        }
+
+        if ( ! yourls_is_shorturl( $keyword ) ) {
             return array(
                 'statusCode' => 404,
-	 	 'status' => 'fail',
-                'simple '    => 'Error: short URL not found',
+                'status'     => 'fail',
+                'simple '    => 'Error: keyword '" . $keyword . "' not found',
                 'message'    => 'error: not found',
             );
         }
@@ -46,17 +52,17 @@
         $title = '';
         if ( isset($_REQUEST['title']) ) $title = $_REQUEST['title'];
 
-        if( yourls_edit_link( $url, $shorturl, $shorturl, $title ) ) {
+        if( yourls_edit_link( $url, $keyword, $keyword, $title ) ) {
             return array(
                 'statusCode' => 200,
-                'simple'     => "Shorturl $shorturl updated to " . $url ,
+                'simple'     => "Keyword $keyword updated to " . $url ,
                 'message'    => 'success: updated',
             );
         } else {
             return array(
                 'statusCode' => 500,
-	 	'status' => 'fail',
-                'simple'     => 'Error: could not edit shorturl, not sure why :-/',
+                'status'     => 'fail',
+                'simple'     => 'Error: could not edit keyword, not sure why :-/',
                 'message'    => 'error: unknown error',
             );
         }
@@ -66,7 +72,7 @@
         if ( ! isset( $_REQUEST['url'] ) ) {
             return array(
                 'statusCode' => 400,
-	 	'status' => 'fail',
+                'status'     => 'fail',
                 'simple'     => "Need a 'url' parameter",
                 'message'    => 'error: missing param',
             );
@@ -85,7 +91,7 @@
         } else {
             return array(
                 'statusCode' => 500,
-	 	'status' => 'fail',
+                'status'     => 'fail',
                 'simple'     => "Error: could not find keyword for url $url",
                 'message'    => 'error: not found',
                 'keyword'    => '',
