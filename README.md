ed0e139247b1fe6e4fe48c6e2ded0f4faf89c7e4
Tim Crockford <tim@crockford.id.au>
2016-01-12 17:43:15 +1100

    Added new change_update keyword to the API designed to change short URLs.

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ yourls-api-edit-url
 YOURLS plugin to add two additional commands to the YOURLS API.
 - update - a function to update the long URL associated with a short code
 - geturl - a function to get the current long URL associated with a short code
+- change_keyword - a function to update the short code associated with a URL
 
 The geturl function does not create a new short code if the URL does not exist, it's purely designed to verify if the URL has been set up. Currently this has not been tested on a site using duplicate URLs.
 
 The update function lets a site update the long URL associated with a short code. There is no security checking on this, as long as the API key is valid it will update, so make sure your key is secure if you plan to use this plugin!
+
+The change_keyword function takes three parameters. "url" is the current URL, "oldshortcode" is the current keyword you want to change, and "newshortcode" is the new keyword you'll change it to. The keyword cannot currently be used in YOURLS.
--- plugin.php
+++ plugin.php
@@ -4,13 +4,15 @@
      Plugin Name: Update Shortened URL
      Plugin URI: https://github.com/timcrockford/yourls-api-edit-url
      Description: Define a custom API action 'update' and 'geturl'
-     Version: 0.2.2
+     Version: 0.2.3
      Author: Tim Crockford
      Author URI: http://codearoundcorners.com/
      */
     
     yourls_add_filter( 'api_action_update', 'api_edit_url_update' );
     yourls_add_filter( 'api_action_geturl', 'api_edit_url_get' );
+    
+    yourls_add_filter( 'api_action_change_keyword', 'api_edit_url_change_keyword' );
     
     function api_edit_url_update() {
         if ( ! isset( $_REQUEST['shorturl'] ) ) {
@@ -56,6 +58,78 @@
             return array(
                 'statusCode' => 200,
                 'simple'     => "Keyword $keyword updated to $url",
+                'message'    => 'success: updated',
+            );
+        } else {
+            return array(
+                'statusCode' => 500,
+                'status'     => 'fail',
+                'simple'     => 'Error: could not edit keyword, not sure why :-/',
+                'message'    => 'error: unknown error',
+            );
+        }
+    }
+
+    function api_edit_url_change_keyword() {
+        if ( ! isset( $_REQUEST['oldshorturl'] ) ) {
+            return array(
+                'statusCode' => 400,
+                'status'     => 'fail',
+                'simple'     => "Need a 'oldshorturl' parameter",
+                'message'    => 'error: missing param',
+            );
+        }
+
+        if ( ! isset( $_REQUEST['newshorturl'] ) ) {
+            return array(
+                'statusCode' => 400,
+                'status'     => 'fail',
+                'simple'     => "Need a 'newshorturl' parameter",
+                'message'    => 'error: missing param',
+            );
+        }
+
+        if ( ! isset( $_REQUEST['url'] ) ) {
+            return array(
+                'statusCode' => 400,
+                'status'     => 'fail',
+                'simple'     => "Need a 'url' parameter",
+                'message'    => 'error: missing param',
+            );
+        }
+
+        $oldshorturl = $_REQUEST['oldshorturl'];
+        $newshorturl = $_REQUEST['newshorturl'];
+        $url = urldecode($_REQUEST['url']);
+
+        if ( yourls_get_protocol( $oldshorturl ) ) {
+            $oldkeyword = yourls_get_relative_url( $oldshorturl );
+        } else {
+            $oldkeyword = $oldshorturl;
+        }
+
+        if ( yourls_get_protocol( $newshorturl ) ) {
+            $newkeyword = yourls_get_relative_url( $newshorturl );
+        } else {
+            $newkeyword = $newshorturl;
+        }
+
+        if ( yourls_keyword_is_taken( $newkeyword ) ) {
+            return array(
+                'statusCode' => 409,
+                'status'     => 'fail',
+                'simple '    => "Error: keyword $newkeyword already exists",
+                'message'    => 'error: already exists',
+            );
+        }
+
+        $title = '';
+        if ( isset($_REQUEST['title']) ) $title = $_REQUEST['title'];
+
+        if( yourls_edit_link( $url, $oldkeyword, $newkeyword, $title ) ) {
+            return array(
+                'statusCode' => 200,
+                'simple'     => "Keyword $oldkeyword updated to $newkeyword for $url",
                 'message'    => 'success: updated',
             );
         } else {
